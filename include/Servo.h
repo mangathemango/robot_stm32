c39762a8ce3606de74afbbe1db748f831203a865
Inkt13 <138111203+Inkt13@users.noreply.github.com>
2026-05-21 14:04:45 +0800

    fix: reimplemented non blocking servos to the main path

--- include/Servo.h
+++ include/Servo.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "main.h"
 
+#define YAW_SERVO 0
+#define CLAW_SERVO 1
+
+#define DEFAULT_TIME 100
+
 // Servo pin macros
 #define SERVO1_HIGH() HAL_GPIO_WritePin(SERVO_1_GPIO_Port,SERVO_1_Pin,GPIO_PIN_SET)
 #define SERVO1_LOW() HAL_GPIO_WritePin(SERVO_1_GPIO_Port,SERVO_1_Pin,GPIO_PIN_RESET)
@@ -14,13 +19,8 @@
 #define SERVO4_HIGH() HAL_GPIO_WritePin(SERVO_4_GPIO_Port, SERVO_4_Pin, GPIO_PIN_SET)
 #define SERVO4_LOW()  HAL_GPIO_WritePin(SERVO_4_GPIO_Port, SERVO_4_Pin, GPIO_PIN_RESET)
 
-#define YAW_SERVO 0
-#define CLAW_SERVO 1
-
-#define DEFAULT_TIME 100
-
 // Function declarations
 void PwmServo_Init(void);
-void PwmServo_Set_Angle(uint8_t index, uint8_t angle);
-void PwmServo_Set_Angle_All(uint8_t a1, uint8_t a2, uint8_t a3, uint8_t a4);
+void PwmServo_Set_Angle(uint8_t index, uint8_t angle, uint16_t time);
+void PwmServo_Set_Angle_All(uint8_t a1, uint8_t a2, uint8_t a3, uint8_t a4, uint16_t time);
 void PwmServo_Handle(void);
